d5cb39fc0c6d06f33d37e885337b05492c40e9ed
Lqz13Th <lqz13th@outlook.com>
2025-12-09 18:40:52 +0800

    Initial project structure

---
+++ rust_mcp_server/target/debug/deps/rust_mcp_server.d
@@ -0,0 +1,5 @@
+C:\quant\work\extrema_mcp_trading_agent\rust_mcp_server\target\debug\deps\rust_mcp_server.d: src\main.rs
+
+C:\quant\work\extrema_mcp_trading_agent\rust_mcp_server\target\debug\deps\rust_mcp_server.exe: src\main.rs
+
+src\main.rs:
---
+++ rust_mcp_server/target/debug/rust_mcp_server.d
@@ -0,0 +1,1 @@
+C:\quant\work\extrema_mcp_trading_agent\rust_mcp_server\target\debug\rust_mcp_server.exe: C:\quant\work\extrema_mcp_trading_agent\rust_mcp_server\src\main.rs
